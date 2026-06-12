d5fcf4d7568b9795a8f199cbc7255173c091b282
ashutoshk2 <ashutosh.kushwaha2@wipro.com>
2026-06-12 12:02:43 +0530

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,49 @@
 'use strict';
 
 const path = require('path');
+const shell = require('shelljs');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 var CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const sgmfScripts = require('sgmf-scripts');
+
+// Local, forward-slash-safe replacements for sgmfScripts.createJsPath()/createScssPath().
+// sgmf-scripts@4 bumped shelljs to 0.10 (fast-glob based); fast-glob treats backslashes
+// as escape characters, so the path.join() globs in sgmf-scripts/lib/helpers.js match
+// nothing on Windows. Building the glob with '/' separators makes it work everywhere.
+const CARTRIDGE = 'int_cybs_sfra';
+const clientDir = path.resolve(`./cartridges/${CARTRIDGE}/cartridge/client`);
+const toPosix = (p) => p.split(path.sep).join('/');
+
+function createJsPath() {
+    const result = {};
+    shell.ls(`${toPosix(clientDir)}/**/js/*.js`).forEach((filePath) => {
+        let location = toPosix(path.relative(clientDir, filePath));
+        location = location.substr(0, location.length - 3);
+        result[location] = filePath;
+    });
+    return result;
+}
+
+function createScssPath() {
+    const result = {};
+    shell.ls(`${toPosix(clientDir)}/**/scss/**/*.scss`).forEach((filePath) => {
+        const name = path.basename(filePath, '.scss');
+        if (name.indexOf('_') !== 0) {
+            let location = toPosix(path.relative(clientDir, filePath));
+            location = location.substr(0, location.length - 5).replace('scss', 'css');
+            result[location] = filePath;
+        }
+    });
+    return result;
+}
 
 
 module.exports = [{
     mode: 'production',
     name: 'js',
-    entry: sgmfScripts.createJsPath(),
+    entry: createJsPath(),
     output: {
         path: path.resolve('./cartridges/int_cybs_sfra/cartridge/static'),
         filename: '[name].js'
@@ -31,7 +62,7 @@ module.exports = [{
 {
     mode: 'none',
     name: 'scss',
-    entry: sgmfScripts.createScssPath(),
+    entry: createScssPath(),
     output: {
         path: path.resolve('./cartridges/int_cybs_sfra/cartridge/static'),
     },
