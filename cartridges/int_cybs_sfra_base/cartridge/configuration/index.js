81aff205c45024d7f039ed624e44136e93cee442
ashutoshk2 <ashutosh.kushwaha2@wipro.com>
2026-06-18 15:07:18 +0530

    psid

--- cartridges/int_cybs_sfra_base/cartridge/configuration/index.js
+++ cartridges/int_cybs_sfra_base/cartridge/configuration/index.js
@@ -17,7 +17,7 @@ var LogfileMaxSize = '5242880'; // 10 MB In Bytes
  * Send this value in all requests that are sent through the partner solution. CyberSource assigns the ID to the partner.
  * Note When you see a partner ID of 999 in reports, the partner ID that was submitted is incorrect.
  */
-var SolutionId = 'AWRA0PP7';
+var SolutionId = '7114dw8t';
 
 var CruiseDDCEndPoint = {
     Stage: 'https://centinelapistag.cardinalcommerce.com/V1/Cruise/Collect',
